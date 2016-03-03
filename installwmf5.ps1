128e971133e0c49a5d7e51ce38ead73a6e4e3383
TigerScript <tony.reynolds@23technology.com>
2016-03-03 17:44:39 +0000

    Added small sleep

--- installwmf5.ps1
+++ installwmf5.ps1
@@ -2,5 +2,7 @@ $source = 'http://go.microsoft.com/fwlink/?LinkId=717507'
 $destination = "$env:temp\wmf5.msu"
 $webclient = New-Object Net.webclient
 $webclient.DownloadFile($source,$destination)
-Start-Process "$destination" -ArgumentList '/quiet' -nonewwindow -wait
+
+Start-Sleep -s 10
+Start-Process "$destination" -ArgumentList '/quiet'
 
